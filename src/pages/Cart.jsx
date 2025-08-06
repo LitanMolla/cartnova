2ad260def4323ec9f2c4fa71abf96447bcd2085a
LitanMolla <litanmolla9@gmail.com>
2025-08-07 04:22:52 +0600

    add to card add on cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,8 +3,22 @@ import Container from '../components/Container'
 import { Link } from 'react-router'
 import { IoClose } from 'react-icons/io5'
 import Btn from '../components/Btn'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
+import { removecartitem } from '../slices/adToCartSlice'
 
 const Cart = () => {
+    let data = useSelector((state) => state.adtocart.value)
+    // console.log(data);
+    let dispatch = useDispatch()
+    let [totalPrice,setTotalPrice]=useState(0)
+    useEffect(()=>{
+        let total = 0;
+        data.map((item)=>{
+            total+=item.price * item.quantity
+        })
+        setTotalPrice(total)
+    },[data])
     return (
         <>
             <section className='py-10 md:py-20'>
@@ -13,7 +27,7 @@ const Cart = () => {
                         <h2 className='text-5xl font-bold mb-3'>Cart</h2>
                         <p className='text-xs text-dark'><Link className='duration-300 hover:text-main hover:font-bold' to='/'>Home</Link>  &gt;  <Link className='duration-300 hover:text-main hover:font-bold' to={'/cart'}>Cart</Link></p>
                     </div>
-                    <div className="overflow-auto">
+                    <div className="overflow-auto mt-12">
                         <table className='border-collapse w-full text-left'>
                             <thead className='border border-light'>
                                 <tr>
@@ -24,26 +38,30 @@ const Cart = () => {
                                 </tr>
                             </thead>
                             <tbody className='border border-light'>
-                                <tr>
-                                    <td className='p-5 border border-light'>
-                                        <div className="flex items-center gap-3 flex-col md:flex-row">
-                                            <button className='cursor-pointer'><IoClose /></button>
-                                            <img className='w-25' src={productImg} alt="dfg" />
-                                            <h4 className='text-lg font-bold'>Product name</h4>
-                                        </div>
-                                    </td>
-                                    <td className='text-xl font-bold p-5 border border-light'>$44.00</td>
-                                    <td className='p-5 border border-light'>
-                                        <div className="border border-light inline-block px-5 py-1">
-                                            <div className="flex gap-5 flex-col md:flex-row">
-                                                <button className='cursor-pointer'>-</button>
-                                                <button>1</button>
-                                                <button className='cursor-pointer'>+</button>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td className='text-xl font-bold p-5 border border-light'>$44.00</td>
-                                </tr>
+                                {
+                                    data.map((item) => (
+                                        <tr>
+                                            <td className='p-5 border border-light'>
+                                                <div className="flex items-center gap-3 flex-col md:flex-row">
+                                                    <button onClick={()=>{dispatch(removecartitem(item))}} className='cursor-pointer'><IoClose /></button>
+                                                    <img className='w-25' src={item.image} alt={item.title} />
+                                                    <h4 className='text-lg font-bold'>{item.title}</h4>
+                                                </div>
+                                            </td>
+                                            <td className='text-xl font-bold p-5 border border-light'>${item.price}</td>
+                                            <td className='p-5 border border-light'>
+                                                <div className="border border-light inline-block px-5 py-1">
+                                                    <div className="flex gap-5 flex-col md:flex-row">
+                                                        <button className='cursor-pointer'>-</button>
+                                                        <button>{item.quantity}</button>
+                                                        <button className='cursor-pointer'>+</button>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td className='text-xl font-bold p-5 border border-light'>${item.price * item.quantity}</td>
+                                        </tr>
+                                    ))
+                                }
                             </tbody>
                         </table>
                     </div>
@@ -52,11 +70,11 @@ const Cart = () => {
                         <div className="max-w-[640px] border border-light border-collapse ml-auto">
                             <div className="flex">
                                 <h5 className='flex-1 p-2 border border-light font-bold'>Subtotal</h5>
-                                <h5 className='flex-1 p-2 border border-light text-dark'>389.99 $</h5>
+                                <h5 className='flex-1 p-2 border border-light text-dark'>${totalPrice.toFixed(2)}</h5>
                             </div>
                             <div className="flex">
                                 <h5 className='flex-1 p-2 border border-light font-bold'>Total</h5>
-                                <h5 className='flex-1 p-2 border border-light '>389.99 $</h5>
+                                <h5 className='flex-1 p-2 border border-light '>${totalPrice.toFixed(2)}</h5>
                             </div>
                         </div>
                         <div className="md:text-end mt-6 text-center">
--- src/slices/adToCartSlice.jsx
+++ src/slices/adToCartSlice.jsx
@@ -7,18 +7,23 @@ export const adToCartSlice = createSlice({
     },
     reducers: {
         addtocart: (state, action) => {
-            console.log(state.value)
-            console.log(action.payload)
             let datacheck = state.value.find((item) => item.id === action.payload.id);
             if (datacheck) {
                 datacheck.quantity++;
             } else {
-                state.value.push({...action.payload,quantity:1})
+                state.value.push({ ...action.payload, quantity: 1 })
             }
+        },
+        removecartitem: (state, action) => {
+            state.value.map((item, index) => {
+                if (item.id === action.payload.id) {
+                    state.value.splice(index, 1)
+                }
+            })
         },
     },
 })
 
-export const { addtocart } = adToCartSlice.actions
+export const { addtocart, removecartitem } = adToCartSlice.actions
 
 export default adToCartSlice.reducer
